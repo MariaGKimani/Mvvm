ffaaf26fcc9c6b49000eb921a331bcd7bbf79c88
MariaGKimani <mariakimanigoretti@gmail.com>
2023-08-24 09:24:56 +0300

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        postViewModel.fetchPosts()
+        postViewModel.errLiveData.observe(this, Observer { err->
+            Toast.makeText(baseContext, err, Toast.LENGTH_LONG).show()
+        })
+
+        postViewModel.postLiveData.observe(this, Observer { productsList->
+            binding.rvRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
+             binding.rvRecycler.layoutManager = GridLayoutManager(this@MainActivity, 1)
+
+
+
+        })
     }
 }
